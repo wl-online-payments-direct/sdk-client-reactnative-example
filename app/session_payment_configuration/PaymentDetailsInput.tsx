53b1b2150e53c389e920ac67e4b5516ca37381f1
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2026-06-09 09:24:34 +0200

    Release 1.0.1

--- app/session_payment_configuration/PaymentDetailsInput.tsx
+++ app/session_payment_configuration/PaymentDetailsInput.tsx
@@ -3,7 +3,7 @@
  *
  * This software is owned by Worldline and may not be be altered, copied, reproduced, republished, uploaded, posted, transmitted or distributed in any way, without the prior written consent of Worldline.
  *
- * Copyright © 2024 Worldline and/or its affiliates.
+ * Copyright © 2026 Worldline and/or its affiliates.
  *
  * All rights reserved. License grant and user rights and obligations according to the applicable license agreement.
  *
